1b318c953b2b6011405bf0c7e5b78fe85110baea
fabian_torres <fabiantfandi@gmail.com>
2026-03-26 14:55:12 -0500

    fix: ruta home

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,12 @@
-from rest_framework import routers
-from .api import ProjectViewSet
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
 
-router = routers.DefaultRouter()
+def home(request):
+    return HttpResponse("API funcionando 🚀")
 
-router.register('api/projects', ProjectViewSet, 'projects')
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('', home),  # 👈 ESTO ARREGLA EL ERROR 500
+    path('admin/', admin.site.urls),
+    path('api/', include('projects.urls')),
+]
