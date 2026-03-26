ed26b68fbc75d36d7af2324e8d367ad88cd03863
fabian_torres <fabiantfandi@gmail.com>
2026-03-26 17:09:15 -0500

    fix: api en raiz

--- projects/urls.py
+++ projects/urls.py
@@ -1,12 +1,8 @@
+
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("API funcionando 🚀")
 
 urlpatterns = [
-    path('', home),  # 👈 ESTO ARREGLA EL ERROR 500
+    path('', include('projects.urls')),  # 👈 esto muestra la API
     path('admin/', admin.site.urls),
-    path('api/', include('projects.urls')),
 ]
