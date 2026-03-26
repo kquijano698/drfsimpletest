c92a63b19f099a2d9ebca234951b64f2c9e7b193
fabian_torres <fabiantfandi@gmail.com>
2026-03-26 17:17:17 -0500

    fix: urls projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,7 @@
+from rest_framework import routers
+from .api import ProjectViewSet
 
-from django.contrib import admin
-from django.urls import path, include
+router = routers.DefaultRouter()
+router.register(r'projects', ProjectViewSet)
 
-urlpatterns = [
-    path('', include('projects.urls')),  # 👈 esto muestra la API
-    path('admin/', admin.site.urls),
-]
+urlpatterns = router.urls
