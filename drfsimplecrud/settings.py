70059d3abe0b59a535cc2e555fa9dec5fe2a92f4
fabian_torres <fabiantfandi@gmail.com>
2026-03-26 16:03:58 -0500

    fix: add session middleware

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -34,10 +34,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    'django.contrib.sessions.middleware.SessionMiddleware',  # 👈 FALTABA ESTO
+
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
